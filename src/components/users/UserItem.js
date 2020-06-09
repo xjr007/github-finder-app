fcf50c2f651c8e74efbe56fbb01502bdd156e40b
Aaqiel <abaaqielbehardien@gmail.com>
2020-06-09 16:23:51 +0200

    from class to functional components

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,35 +1,31 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-export class UserItem extends Component {
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+    return (
+        <div className='card text-center'>
+            <img 
+                src={avatar_url} 
+                alt='avatar'
+                className='round-img' 
+                style={{width: '60px' }}
+            />
+            <h3>{login}</h3>
 
-    render() {
-        //the const below is destructuring
-        /*
-        This means that I am able to 'pull' the properties
-        from the object chosen.
-        */
-        const {login, avatar_url, html_url} = this.props.user;
-        return (
-            <div className='card text-center'>
-                <img 
-                    src={avatar_url} 
-                    alt='avatar'
-                    className='round-img' 
-                    style={{width: '60px' }}
-                />
-                <h3>{login}</h3>
-
-                <div>
-                    <a 
-                        href={html_url}
-                        className="btn btn-dark btn-sm my-1">
-                        More
-                    </a>
-                </div>
-
+            <div>
+                <a 
+                    href={html_url}
+                    className="btn btn-dark btn-sm my-1">
+                    More
+                </a>
             </div>
-        )
-    }
+
+        </div>
+    )
 }
 
-export default UserItem
+UserItem.propTypes = {
+    user: PropTypes.object.isRequired
+}
+
+export default UserItem;
