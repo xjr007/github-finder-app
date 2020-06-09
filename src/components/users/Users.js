66f3ee721b041d0f20c04cb182e38d94d2d7b101
Aaqiel <abaaqielbehardien@gmail.com>
2020-06-09 17:20:38 +0200

    Updating Requesting States

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,32 +2,10 @@ import React, { Component } from 'react';
 import UserItem from './UserItem';
 
 class Users extends Component {
-    state = {
-        users: [
-            {
-                id: '1',
-                login: 'mojombo',
-                avatar_url: '#',
-                html_url: '#'
-            },
-            {
-                id: '2',
-                login: 'drkeisw',
-                avatar_url: '#',
-                html_url: '#'
-            },
-            {
-                id: '3',
-                login: 'gtkeodalq',
-                avatar_url: '#',
-                html_url: '#'
-            }
-        ]
-    }
     render() {
         return (
             <div style={userStyle}>
-                {this.state.users.map(user => (
+                {this.props.users.map(user => (
                     <UserItem key={user.id} user={user}/>
                 ))}
             </div>
