8da7c5062e73029a7968d95863db56d01aee7189
Aaqiel <abaaqielbehardien@gmail.com>
2020-06-10 13:51:44 +0200

    alert added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, {Component} from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   }
 
   searchUsers = async (text) => {
@@ -23,6 +25,12 @@ class App extends Component {
 
   clearUsers = () => this.setState({ users: [], loading: false });
 
+  setAlert = (msg, type) => {
+    this.setState({alert: { msg, type }});
+
+    setTimeout(() => this.setState({ alert: null }), 5000);
+  }
+
   render() {
 
     const {users, loading} = this.state;
@@ -31,9 +39,12 @@ class App extends Component {
       <div className="App">
         <Navbar title="Github Finder" icon="fab fa-github" />
         <div className="container">
+          <Alert alert={this.state.alert} />
           <Search searchUsers={this.searchUsers} 
           clearUsers={this.clearUsers} 
-          showClear={ users.length > 0 ? true : false }/>
+          showClear={ users.length > 0 ? true : false }
+          setAlert={this.setAlert}
+          />
 
           <Users loading={loading} users={users} />
         </div>
