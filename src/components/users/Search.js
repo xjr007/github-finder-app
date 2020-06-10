7a70d0315214bc2b7d0254b86d92bd24401f534a
Aaqiel <abaaqielbehardien@gmail.com>
2020-06-10 11:27:05 +0200

    events and search

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -4,11 +4,25 @@ class Search extends Component {
     state = {
         text: ''
     }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state.text);
+    }
+
+    onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
     render() {
         return (
             <div>
-                <form className='form'>
-                    <input type="text" name="text" placeholder="Search Users..." value={this.state.text} />
+                <form onSubmit={this.onSubmit} className='form'>
+                    <input 
+                    type="text" 
+                    name="text" 
+                    placeholder="Search Users..." 
+                    value={this.state.text} 
+                    onChange={this.onChange}
+                    />
                     <input type="submit" value="Search" className="btn btn-dark btn-block"/>
                 </form>
             </div>
