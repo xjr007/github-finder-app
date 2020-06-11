7548fbcdfa5127b552907b514e344a75f0bc84e2
Aaqiel <abaaqielbehardien@gmail.com>
2020-06-11 11:58:21 +0200

    Changed Search.js to functional

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,43 +1,31 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-class Search extends Component {
-    state = {
-        text: ''
-    }
+const Search = ({searchUsers, showClear, clearUsers, setAlert }) => {
+    const [text, setText] = useState('');
 
-    static propTypes = {
-        searchUsers: PropTypes.func.isRequired,
-        clearUsers: PropTypes.func.isRequired,
-        showClear: PropTypes.bool.isRequired,
-        setAlert: PropTypes.func.isRequired
-    }
-
-    onSubmit = (e) => {
+    const onSubmit = e => {
         e.preventDefault();
-        if(this.state.text === '') {
-            this.props.setAlert('Please enter something', 'Light');
+        if(text === '') {
+            setAlert('Please enter something', 'Light');
         } else {
-            this.props.searchUsers(this.state.text);
-            this.setState({ text: '' });}
+            searchUsers(text);
+            setText('');
+        }
         
     }
 
-    onChange = (e) => this.setState({ [e.target.name]: e.target.value });
-
-    render() {
-
-        const { showClear, clearUsers } = this.props;
+    const onChange = (e) => setText( e.target.value );
 
         return (
             <div>
-                <form onSubmit={this.onSubmit} className='form'>
+                <form onSubmit={onSubmit} className='form'>
                     <input 
                     type="text" 
                     name="text" 
                     placeholder="Search Users..." 
-                    value={this.state.text} 
-                    onChange={this.onChange}
+                    value={text} 
+                    onChange={onChange}
                     />
                     <input type="submit" value="Search" className="btn btn-dark btn-block"/>
                 </form>
@@ -51,8 +39,14 @@ class Search extends Component {
                 )}
                 
             </div>
-        )
-    }
+        );
+};
+
+Search.propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired
 }
 
 export default Search;
